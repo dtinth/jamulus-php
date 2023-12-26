e41036e89511cc77e7035ec1b2a011c91236937e
Thai Pangsakulyanont <dtinth@spacet.me>
2023-12-26 12:01:27 +0000

    fix error handling

--- src/getAllServers.ts
+++ src/getAllServers.ts
@@ -1,17 +1,20 @@
 import { execa } from "execa";
 
-export async function fetchServers(directory: string) {
-  return await execa("php", ["servers.php", `directory=${directory}`], {
+export async function fetchServer(directory: string) {
+  const result = await execa("php", ["servers.php", `directory=${directory}`], {
     timeout: 10000,
   });
-}
-
-export async function getAllServers() {
-  const result = await fetchServers("anygenre1.jamulus.io:22124");
   try {
     return JSON.parse(result.stdout);
   } catch (e) {
-    console.error("Unable to parse JSON output. Raw output:", result.stdout);
+    console.error(
+      `Unable to parse JSON output when parsing directory: ${directory}`
+    );
+    console.error(`Received output: ${JSON.stringify(result.stdout)}`);
     throw e;
   }
+}
+
+export async function getAllServers() {
+  return await fetchServer("anygenre1.jamulus.io:22124");
 }
