1dd7e14b55074af1f7ba11073c4cc806b9d872c5
Thai Pangsakulyanont <dtinth@spacet.me>
2023-12-26 12:06:10 +0000

    all the servers

--- src/getAllServers.ts
+++ src/getAllServers.ts
@@ -1,4 +1,15 @@
 import { execa } from "execa";
+import pMap from "p-map";
+
+const explorerUrls = {
+  "Any Genre 1": "anygenre1.jamulus.io:22124",
+  "Any Genre 2": "anygenre2.jamulus.io:22224",
+  "Any Genre 3": "anygenre3.jamulus.io:22624",
+  "Genre Rock": "rock.jamulus.io:22424",
+  "Genre Jazz": "jazz.jamulus.io:22324",
+  "Genre Classical/Folk": "classical.jamulus.io:22524",
+  "Genre Choral/Barbershop": "choral.jamulus.io:22724",
+};
 
 export async function fetchServer(directory: string) {
   const result = await execa("php", ["servers.php", `directory=${directory}`], {
@@ -16,5 +27,9 @@ export async function fetchServer(directory: string) {
 }
 
 export async function getAllServers() {
-  return await fetchServer("anygenre1.jamulus.io:22124");
+  const time = new Date().toISOString();
+  return pMap(Object.entries(explorerUrls), async ([genre, server]) => {
+    const list = await fetchServer(server).catch((e) => []);
+    return { time, list, genre };
+  });
 }
