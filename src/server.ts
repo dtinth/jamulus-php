e4a83893e324085fa83e9e300860a9a273a5d827
Thai Pangsakulyanont <dtinth@spacet.me>
2023-12-26 12:07:46 +0000

    check api key

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,12 @@ import { getAllServers } from "./getAllServers";
 const fastify = Fastify({ logger: true });
 
 fastify.get("/", async function handler(request, reply) {
+  const apiKey = process.env.API_KEY;
+  if (!apiKey || request.headers["x-api-key"] !== apiKey) {
+    reply.status(401).send("Unauthorized");
+    return;
+  }
+
   return {
     data: await getAllServers(),
   };
